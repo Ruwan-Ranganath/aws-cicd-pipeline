d921589edc12554c22289afdb27bb2f29da3593f
Ruwan Ranganath <Ruwan-Ranganath@users.noreply.github.com>
2021-10-15 14:39:43 +0530

    iam-update

---
+++ iam-pipeline.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "tf-codepipeline-role" {
+  name = "tf-codepipeline-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+
+data "aws_iam_policy_document" "tf-cicd-pipeline-policies" {
+    statement{
+        sid = ""
+        actions = ["codestar-connections:UseConnection"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+    statement{
+        sid = ""
+        actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+}
+
+resource "aws_iam_policy" "tf-cicd-pipeline-policy" {
+    name = "tf-cicd-pipeline-policy"
+    path = "/"
+    description = "Pipeline policy"
+    policy = data.aws_iam_policy_document.tf-cicd-pipeline-policies.json
+}
+
+resource "aws_iam_role_policy_attachment" "tf-cicd-pipeline-attachment" {
+    policy_arn = aws_iam_policy.tf-cicd-pipeline-policy.arn
+    role = aws_iam_role.tf-codepipeline-role.id
+}
